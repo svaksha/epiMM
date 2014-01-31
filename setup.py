7af4db33b7b5e62c1e911c102f83fb28be98eee0
SVAKSHA <svaksha@gmail.com>
2014-01-31 17:46:38 +0530

    requirements.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
 from __future__ import (absolute_import, division,print_function, unicode_literals)
 ################################################################################
 """
-COPYRIGHT: 2013-Now, SVAKSHA :: https://github.com/svaksha
+COPYRIGHT(C) 2013-Now SVAKSHA :: https://github.com/svaksha
 LICENSE: AGPLv3 License <http://www.gnu.org/licenses/agpl.html>.
-All copies must retain this permission notice with the copyright notice.
+# All copies must retain this permission notice with the copyright notice.
 """
 ################################################################################
-
 __author__ = 'SVAKSHA'
 __copyright__ = 'Copyright (c) 2013-Now, SVAKSHA'
 __license__ = 'AGPLv3'
+__version__ = "14.02.dev"
+
 from distutils.core import setup
 #from setuptools import setup, find_packages
 import os, sys
--- setup_egg.py
+++ setup_egg.py
@@ -2,10 +2,17 @@
 # encoding: utf-8
 from __future__ import (absolute_import, division,print_function, unicode_literals)
 ################################################################################
-# Copyright(C) 2013-Now SVAKSHA:: https://github.com/svaksha
-# Distributed under the AGPLv3 License <http://www.gnu.org/licenses/agpl.html>.
+"""
+COPYRIGHT(C) 2013-Now SVAKSHA :: https://github.com/svaksha
+LICENSE: AGPLv3 License <http://www.gnu.org/licenses/agpl.html>.
 # All copies must retain this permission notice with the copyright notice.
+"""
 ################################################################################
+__author__ = 'SVAKSHA'
+__copyright__ = 'Copyright (c) 2013-Now, SVAKSHA'
+__license__ = 'AGPLv3'
+__version__ = "14.02.dev"
+
 
 from setuptools import setup
 
